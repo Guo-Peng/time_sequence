d05de29fd7aa75936699bb4f2366fc2f36367d78
guopeng <guopeng@gitlab.local>
2016-04-15 15:42:37 +0800

    annotation

--- basic.py
+++ basic.py
@@ -3,11 +3,13 @@ import theano
 import theano.tensor as T
 import numpy as np
 
+# 变量定义
 foo = T.scalar('foo')
 bar = foo ** 2
 f = theano.function([foo], bar)
 
 
+# 共享变量
 shared_var = theano.shared(np.array([[1, 2], [2, 3]], dtype=float))
 shared_square = shared_var ** 2
 function_1 = theano.function([], shared_square)
@@ -21,6 +23,7 @@ print shared_var.get_value()
 print function_1()
 
 
+# 梯度计算
 fun_grad = T.grad(bar, foo)
 print fun_grad.eval({foo: 20})
 
@@ -29,6 +32,7 @@ A = T.matrix('A')
 x = T.vector('x')
 b = T.vector('b')
 y = T.dot(A, x) + b
+# 也可以为hessian
 y_j = theano.gradient.jacobian(y, x)
 y_j_f = theano.function([A, x, b], y_j)
 print y_j_f(np.array([[9, 8, 7], [4, 5, 6]], dtype=theano.config.floatX),  # A
@@ -36,6 +40,7 @@ print y_j_f(np.array([[9, 8, 7], [4, 5, 6]], dtype=theano.config.floatX),  # A
             np.array([4, 5], dtype=theano.config.floatX))
 
 
+# test value
 theano.config.compute_test_value = 'warn'
 A = T.matrix('A')
 B = T.matrix('B')
@@ -44,6 +49,7 @@ B.tag.test_value = np.random.random((5, 6)).astype(theano.config.floatX)
 C = T.dot(A, B)
 
 
+# debug mode 不允许nan inf值的出现
 num = T.scalar('num')
 den = T.scalar('den')
 f = theano.function([num, den], num / den, mode='DebugMode')
