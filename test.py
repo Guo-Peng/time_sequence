ad0cfecd58404c26c09d599ef9a4b75fbfcd1caf
郭鹏 <532957426@qq.com>
2016-05-19 20:30:42 +0800

    tensorflow demo

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ import pandas as pd
 from sklearn.preprocessing import scale
 
 binary = np.unpackbits(np.array([range(10)], dtype=np.uint8).T, axis=1)
-print np.array([range(10)], dtype=np.uint8).T
+print np.array([[1]]).T
