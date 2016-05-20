b51d25cff394cfc737ee078110f894a95aaeb1e4
郭鹏 <532957426@qq.com>
2016-05-20 16:19:25 +0800

    add a layer and encounter some problem(accurancy = 0.1135)

--- tensorflow/mnist.py
+++ tensorflow/mnist.py
@@ -26,22 +26,30 @@
 # 可交互版本
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+
 mnist = input_data.read_data_sets("/Users/Peterkwok/GitHub/time_sequence/tensorflow/MNIST_data/", one_hot=True)
 
 x = tf.placeholder(tf.float32, [None, 784])
 y_ = tf.placeholder(tf.float32, [None, 10])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+
+W = tf.Variable(tf.zeros([784, 100]))
+b = tf.Variable(tf.zeros([100]))
+
+W_2 = tf.Variable(tf.zeros([100, 10]))
+b_2 = tf.Variable(tf.zeros([10]))
+
+y_1 = tf.matmul(x, W) + b
 
 sess = tf.InteractiveSession()
 sess.run(tf.initialize_all_variables())
-y = tf.nn.softmax(tf.matmul(x,W) + b)
+y = tf.nn.softmax(tf.matmul(y_1, W_2) + b_2)
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 for i in range(1000):
-  batch = mnist.train.next_batch(100)
-  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+    batch = mnist.train.next_batch(50)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
