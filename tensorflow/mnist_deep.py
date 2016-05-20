29956a9424ebf8fd94d7fe6b353b61502c1f2d24
郭鹏 <532957426@qq.com>
2016-05-20 15:58:11 +0800

    interactive version

---
+++ tensorflow/mnist_deep.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.initialize_all_variables())
